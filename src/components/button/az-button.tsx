18527c4cf94a03ad328a4e2a23d904910229a25b
wangweihua <wangweihua@vipkid.com.cn>
2019-09-04 19:45:49 +0800

    :recycle: (az-button) shadow:false

--- src/components/button/az-button.tsx
+++ src/components/button/az-button.tsx
@@ -1,12 +1,12 @@
 import { Component, Prop, Element, h } from '@stencil/core';
 import { ComponentStyleType} from '../../global/typing';
-import { migrateAttributes } from '../../utils/utils';
 import { HostElement } from '@stencil/core/dist/declarations';
+import { Inject } from '../../utils/utils';
 
 @Component({
   tag: 'az-button',
   styleUrl: 'az-button.styl',
-  shadow: true
+  shadow: false
 })
 export class AzButton {
   @Element() el: HostElement;
@@ -15,16 +15,17 @@ export class AzButton {
    */
   @Prop() type: ComponentStyleType = 'plain';
 
-  componentDidLoad() {
-    migrateAttributes(this.el);
-  }
+  @Inject({
+    attrs: true
+  })
+  componentDidLoad() {}
 
   render() {
     return (
       <button class={{
         'az-button': true,
         [this.type]: true
-      }} {...this.el.attributes}>
+      }}>
         <slot name="before"></slot>
         <span class="caption">
           <slot></slot>
