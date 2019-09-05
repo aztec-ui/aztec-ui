652b60243c9e25717c120be9b4ec66e78c1778e1
wangweihua <wangweihua@vipkid.com.cn>
2019-09-05 20:53:37 +0800

    :sparkles: (az-icon) add az-icon

---
+++ src/components/icons/az-icon.tsx
@@ -0,0 +1,24 @@
+import { Component, Prop} from '@stencil/core';
+import icons from './builtin';
+
+@Component({
+  tag: 'az-icon',
+  styleUrl: 'az-icon.styl',
+  shadow: false
+})
+export class AzIcon {
+  static icons = icons;
+
+  @Prop() icon: string = '';
+  @Prop() width: number = 12;
+  @Prop() height: number = 12;
+  @Prop() color: string = 'white';
+
+  render () {
+    const icon = AzIcon.icons[this.icon];
+    if (typeof icon === 'undefined') {
+      throw new Error('Can not find icon `${this.icon}`');
+    }
+    return icon(this.width, this.height, this.color);
+  }
+}
---
+++ src/components/icons/builtin/arrow-right.tsx
@@ -0,0 +1,9 @@
+import {h} from '@stencil/core';
+export default function (width = 12, height = 12, fill="#fff") {
+  return (
+    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 1152 1024">
+      <path fill={fill} d="M624.576 0 544.128 80.512 976.192 512.032 544.128 943.488 624.576 1024 1136.096 512.032Z"></path>
+    </svg>
+  );
+}
+
