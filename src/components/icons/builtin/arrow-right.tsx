2159d1d2b8824fded2e26443a1473f218a63d965
wangweihua <wangweihua@vipkid.com.cn>
2019-09-08 21:12:09 +0800

    :sparkles: (icons/builtin) add many builtin icons

--- src/components/icons/builtin/arrow-right.tsx
+++ src/components/icons/builtin/arrow-right.tsx
@@ -1,9 +1,2 @@
-import {h} from '@stencil/core';
-export default function (width = 12, height = 12, fill="#fff") {
-  return (
-    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 1152 1024">
-      <path fill={fill} d="M624.576 0 544.128 80.512 976.192 512.032 544.128 943.488 624.576 1024 1136.096 512.032Z"></path>
-    </svg>
-  );
-}
+export default "M384.213333 170.666667l341.333334 341.333333-341.333334 341.333333L298.666667 768l256.213333-256L298.666667 256z";
 
