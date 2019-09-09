9b188a4a1e9ba5bd28845124feb8d2abaecc02cb
wangweihua <wangweihua@vipkid.com.cn>
2019-09-09 18:38:31 +0800

    :construction: (az-tree) WIP

---
+++ src/components/tree/az-tree-item.tsx
@@ -0,0 +1,60 @@
+
+import { Component, Prop, Element, h, Method } from '@stencil/core';
+import { HostElement } from '@stencil/core/dist/declarations';
+import { AzTree } from './az-tree';
+var id = 1;
+@Component({
+  tag: 'az-tree-item',
+  styleUrl: 'az-tree-item.styl',
+  shadow: false
+})
+export class AzTreeItem {
+  @Element() el: HostElement;
+
+  @Prop() caption: string = '';
+  @Prop() children: AzTreeItem[] = [];
+  @Prop() selected: false;
+  @Prop() tree: AzTree = null;
+  @Prop() level: number = 0;
+  @Prop() expanded: boolean = false;
+
+  data: any = null;
+
+  constructor(caption: string, level: number = 0) {
+    // @ts-ignore
+    if (typeof hostRef !== 'undefined' && hostRef.$instanceValues$) {
+      // @ts-ignore
+      this.caption = hostRef.$instanceValues$.get('caption');
+      // @ts-ignore
+      this.level = hostRef.$instanceValues$.get('level');
+    } else {
+      this.caption = caption;
+      this.level = level;
+    }
+  }
+
+  @Method()
+  async addItem(item: AzTreeItem | string) {
+    if (!this.tree) {
+      throw new Error(`No parent tree!`);
+    }
+    this.tree.addItem(item, this);
+  }
+
+  render() {
+    const style: any = {};
+    if (this.level) style.textIndent = `${(this.level + 1) * 16}px`;
+    return (
+      <div id={`az-tree-item-${id++}`} class={{'az-tree-item': true, 'expanded': this.expanded}} data-level={this.level} style={style}>
+        <div class="az-tree-item-caption az-caption">
+          {this.children.length > 0 && <az-icon class="joint" width="9" height="9" icon="plus"></az-icon>}
+          {this.tree && this.tree.selecting && <az-checkbox></az-checkbox>}
+          <span class="az-tree-item-caption az-caption">{this.caption}</span>
+        </div>
+        {this.children.length > 0 && <div class="az-tree-children">{
+          this.children.map((c: AzTreeItem) => <az-tree-item caption={c.caption} level={c.level}></az-tree-item>)
+        }</div>}
+      </div>
+    );
+  }
+}
---
+++ src/components/tree/az-tree.tsx
@@ -0,0 +1,45 @@
+
+import { Component, Prop, Element, h, Method } from '@stencil/core';
+import { HostElement } from '@stencil/core/dist/declarations';
+import { AzTreeItem } from './az-tree-item';
+
+@Component({
+  tag: 'az-tree',
+  styleUrl: 'az-tree.styl',
+  shadow: false
+})
+export class AzTree {
+  @Element() el: HostElement;
+  @Prop() caption: string = '';
+  @Prop() selecting: boolean = false;
+  @Prop() roots: AzTreeItem[] = [];
+
+  @Method()
+  async addItem(itemOrCaption: AzTreeItem | string, parent: AzTreeItem = null) {
+    let item: AzTreeItem;
+    if (typeof itemOrCaption === 'string') {
+      item = new AzTreeItem(itemOrCaption);
+    } else if (itemOrCaption instanceof AzTreeItem){
+      item = itemOrCaption;
+    }
+    if (!item) return;
+    item.tree = this;
+    if (!parent) {
+      if (!this.roots.length) {
+        this.roots = [...this.roots, item];
+        return;
+      }
+      parent = this.roots[this.roots.length - 1];
+    }
+    item.level = parent.level + 1;
+    parent.children = [...parent.children, item];
+    this.el.forceUpdate();
+    return item;
+  }
+
+  componentDidLoad() {}
+
+  render() {
+    return this.roots.map((c: AzTreeItem) => <az-tree-item data-id="0">{c.render()}</az-tree-item>);
+  }
+}
