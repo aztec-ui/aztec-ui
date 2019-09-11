5988f271d7ff5d6985bdc81ba0eaa3c4cf455400
wangweihua <wangweihua@vipkid.com.cn>
2019-09-11 13:21:48 +0800

    :sparkles: (az-slider) add az-slider

---
+++ src/components/slider/az-slider.tsx
@@ -0,0 +1,28 @@
+
+import { Component, Prop, Element, h } from '@stencil/core';
+import { HostElement } from '@stencil/core/dist/declarations';
+
+@Component({
+  tag: 'az-slider',
+  styleUrl: 'az-slider.styl',
+  shadow: false
+})
+export class AzSlider {
+  @Element() el: HostElement;
+
+  @Prop() caption: string = '';
+
+  componentDidLoad() {}
+
+  render() {
+    const vdom = [
+      <slot name="before"></slot>,
+      <input type="range"></input>,
+      <slot name="after"></slot>,
+    ];
+    if (this.caption) {
+      vdom.unshift(<span class="az-slider-caption az-caption">{this.caption}</span);
+    }
+    return vdom;
+  }
+}
