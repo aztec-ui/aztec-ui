108c50285ce759342df771e54216a8c8f7162cda
wangweihua <wangweihua@vipkid.com.cn>
2019-09-17 19:48:18 +0800

    :recycle: (az-tooltip) rewrite tooltip without popover

--- src/components/tooltip/az-tooltip.tsx
+++ src/components/tooltip/az-tooltip.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, Element, h } from '@stencil/core';
+import { Component, Prop, Element, h, Host } from '@stencil/core';
 import { HostElement } from '@stencil/core/dist/declarations';
 
 @Component({
@@ -9,18 +9,82 @@ import { HostElement } from '@stencil/core/dist/declarations';
 export class AzTooltip {
   @Element() el: HostElement;
 
-  @Prop() placement: 'top' | 'bottom' | 'left' | 'right' = 'top';
-  @Prop() caption: string = '';
+  @Prop({reflect: true}) placement: 'top' | 'bottom' | 'left' | 'right' = 'top';
+  @Prop({reflect: true}) caption: string = '';
+  @Prop({reflect: true}) trigger: 'hover' | 'click' | 'manual' = 'hover';
+  @Prop({reflect: true}) isShow: boolean = false;
+  @Prop({reflect: true}) delay: number = 0;
   popover: HTMLElement;
+  container: HTMLElement;
+  timerId: number;
 
   componentDidLoad() {
-    this.popover.classList.add(this.placement);
-    this.el.parentElement.append(this.popover);
-    this.el.remove();
-    this.popover.parentElement.style.position = 'relative';
+    this.el.classList.add(this.placement);
+    this.container = this.el.parentElement;
+    this.el.parentElement.style.position = 'relative';
+
+    this.onMouseEnterContainer = this.onMouseEnterContainer.bind(this);
+    this.onMouseLeaveContainer = this.onMouseLeaveContainer.bind(this);
+    this.onClickContainer = this.onClickContainer.bind(this);
+    this.onClickOutside = this.onClickOutside.bind(this);
+
+    this.el.style.display = this.isShow ? '' : 'none';
+    this.bind();
+  }
+
+  bind() {
+    this.container.addEventListener('click', this.onClickContainer);
+    this.container.addEventListener('mouseenter', this.onMouseEnterContainer);
+    this.container.addEventListener('mouseleave', this.onMouseLeaveContainer);
+    document.addEventListener('click', this.onClickOutside);
+  }
+
+  onMouseEnterContainer() {
+    if (this.trigger !== 'hover') return;
+    this.show();
+  }
+  onMouseLeaveContainer() {
+    if (this.trigger !== 'hover') return;
+    this.hide();
+  }
+  onClickContainer(e: MouseEvent) {
+    if (this.trigger !== 'click' && this.trigger !== 'manual') return;
+    this.toggle();
+    e.stopPropagation();
+  }
+  onClickOutside() {
+    if (this.trigger === 'manual') return;
+    this.hide();
+  }
+
+  toggle(force: boolean | undefined = undefined) {
+    const s = this.el.style;
+    const condition = typeof force === 'undefined'
+      ? s.display === 'none'
+      : force;
+    const val = condition ? '' : 'none';
+    s.display = val;
+  }
+
+  show () {
+    if (this.delay > 0) {
+      this.timerId = window.setTimeout(() => this.toggle(true), this.delay);
+    } else {
+      this.toggle(true);
+    }
+  }
+  hide() {
+    window.clearTimeout(this.timerId);
+    this.timerId = 0;
+    this.toggle(false);
   }
 
   render() {
-    return <az-popover class="az-tooltip" ref={el => this.popover = el} inline={true}>{this.caption}</az-popover>;
+    return (
+      <Host class="az-tooltip">
+        {this.caption}
+        <slot></slot>
+      </Host>
+    );
   }
 }
