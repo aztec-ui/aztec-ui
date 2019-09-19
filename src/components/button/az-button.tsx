d1e7cfe3dc9023ba81a371d6d9934c4bce5141e1
wangweihua <wangweihua@vipkid.com.cn>
2019-09-19 16:31:42 +0800

    :recycle: (az-button) add ComponentStyles for az-button

--- src/components/button/az-button.tsx
+++ src/components/button/az-button.tsx
@@ -1,5 +1,5 @@
-import { Component, Prop, Element, h } from '@stencil/core';
-import { ComponentStyleType} from '../../global/typing';
+import { Component, Prop, Element, Host, h } from '@stencil/core';
+import { ComponentStyle, ComponentSize} from '../../global/typing';
 import { HostElement } from '@stencil/core/dist/declarations';
 import { Inject } from '../../utils/utils';
 
@@ -13,27 +13,44 @@ export class AzButton {
   /**
    * Button type
    */
-  @Prop() type: ComponentStyleType = '';
-
-  @Prop() caption: string = '';
+  @Prop({reflect: true}) type: ComponentStyle = 'primary';
+  @Prop({reflect: true}) icon: string = '';
+  @Prop({reflect: true}) size: ComponentSize = 'normal';
+  @Prop({reflect: true}) caption: string = '';
+  @Prop({reflect: true}) iconPosition: string = 'left';
+  @Prop({reflect: true}) disabled: boolean = false;
 
   @Inject({
     attrs: true,
     remove: true,
-    keep: ['caption', 'type']
+    keep: ['caption', 'type', 'size', 'icon', 'icon-position']
   })
-  componentDidLoad() {}
+  componentDidLoad() {
+    if (!this.type) this.type = 'primary';
+    if (!this.size) this.size = 'normal';
+    if (!this.iconPosition) this.iconPosition = 'left';
+    if (this.disabled == null) this.disabled = false;
+  }
 
   render() {
+    let iconLeft = this.icon && this.iconPosition === 'left' ? <az-icon class="left" icon={this.icon}></az-icon> : null;
+    let iconRight = this.icon && this.iconPosition === 'right' ? <az-icon class="right" icon={this.icon}></az-icon> : null;
+    if (this.size === 'extra-small') {
+      iconLeft = iconRight = null;
+      console.warn(`<az-button size="extra-small" /> can not have icon`);
+    }
     return (
-      <button class={{
-        [this.type]: true
-      }}>
-        <slot name="before"></slot>
-        {this.caption && <span class="az-button-caption az-caption">{this.caption}</span>}
-        <slot></slot>
-        <slot name="after"></slot>
-      </button>
+      <Host>
+        <button class={{
+          [this.type]: true,
+          [this.size]: true
+        }}>
+          <slot name="before">{iconLeft}</slot>
+          {this.caption && <span class="az-button-caption az-caption">{this.caption}</span>}
+          <slot></slot>
+          <slot name="after">{iconRight}</slot>
+        </button>
+      </Host>
     );
   }
 }
