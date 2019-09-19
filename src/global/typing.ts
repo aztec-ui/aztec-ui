fe98b14bb94557ca40a872ab6fb4fee49da4afdb
wangweihua <wangweihua@vipkid.com.cn>
2019-09-19 16:23:36 +0800

    :recycle: (typing) rename type name and align enum with css name

--- src/global/typing.ts
+++ src/global/typing.ts
@@ -1,1 +1,2 @@
-export type ComponentStyleType = '' | 'plain' | 'primary' | 'success' | 'info' | 'warning' | 'danger';
+export type ComponentStyle = 'plain' | 'primary' | 'success' | 'info' | 'warning' | 'danger';
+export type ComponentSize = 'extra-small' | 'small' | 'normal' | 'medium' | 'large' | 'extra-large';
