6eb236793e10713696a1f8ba5b2a7e12dc04e589
wangweihua <wangweihua@vipkid.com.cn>
2019-10-13 11:57:39 +0800

    :recycle: (az-splitter) read initial flexBasis from style width or height/flexBasis

--- src/components/splitter/az-splitter.tsx
+++ src/components/splitter/az-splitter.tsx
@@ -24,8 +24,9 @@ export class AzSpliter {
 
     this.childrenEles = Array.from(this.el.children);
     const r = (100 / this.childrenEles.length).toFixed(6) + '%';
+    const prop = this.direction === 'vertical' ? 'height' : 'width';
     this.childrenEles.forEach((child: HTMLElement, index: number) => {
-      const w = child.style.width || child.style.flexBasis;
+      const w = child.style[prop] || child.style.flexBasis;
       child.style.flex = `0 0 ${w || r}`;
       if (index === this.childrenEles.length - 1) return;
       const handle = document.createElement('div');
