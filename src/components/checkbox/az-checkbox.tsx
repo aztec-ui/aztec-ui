ad79c7106f48476c5552e99a17743afa2da34756
wangweihua <wangweihua@vipkid.com.cn>
2019-10-15 17:37:42 +0800

    :recycle: (az-checkbox) toggle on click caption

--- src/components/checkbox/az-checkbox.tsx
+++ src/components/checkbox/az-checkbox.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Prop, h, Event, EventEmitter} from '@stencil/core';
+import { Component, Element, Prop, Host, h, Event, EventEmitter} from '@stencil/core';
 import { HostElement } from '@stencil/core/dist/declarations';
 import { Inject } from '../../utils/utils';
 
@@ -31,19 +31,19 @@ export class AzCheckbox {
   }
 
   render () {
-    return [
+    return <Host onClick={() => this.toggle()}>
       <i class={{
         'az-checkbox-box': true,
         'checked': this.checked
-      }} onClick={() => this.toggle()}>
+      }}>
         {this.indeterminate && <az-icon class="minus" icon="minus"></az-icon>}
         {this.indeterminate || <az-icon class="check" icon="check"></az-icon>}
-      </i>,
+      </i>
       <slot>
-        <span class="az-checkbox-caption az-caption" onClick={() => this.toggle()}>
+        <span class="az-checkbox-caption az-caption">
           {this.caption}
         </span>
       </slot>
-    ];
+    </Host>;
   }
 }
