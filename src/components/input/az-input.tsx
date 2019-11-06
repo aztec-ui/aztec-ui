5110c64752fe165fb79a78da4643410326a4d6d4
wangweihua <wangweihua@vipkid.com.cn>
2019-11-06 14:26:43 +0800

    :sparkles: (az-input) add clearable attr

--- src/components/input/az-input.tsx
+++ src/components/input/az-input.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, Element, h, Watch} from '@stencil/core';
+import { Component, Prop, Element, h, Watch, Host} from '@stencil/core';
 import { Inject } from '../../utils/utils';
 import { HostElement } from '@stencil/core/dist/declarations';
 
@@ -12,21 +12,28 @@ export class AzInput {
   @Prop() caption: string = '';
   @Prop() type: string = '';
   @Prop() value: string = '';
-  @Prop() native: HTMLInputElement;
+  @Prop({reflect: true}) clearable = false;
 
-  @Prop() autocomplete: string;
-  @Prop() autocorrect: string;
-  @Prop() autocapitalize: string;
+  @Prop() autocomplete: string = 'off';
+  @Prop() autocorrect: string = 'off';
+  @Prop() autocapitalize: string = 'off';
   @Prop() spellcheck: boolean = true;
+
+  native: HTMLInputElement;
+  clearButton: HTMLAzIconElement;
 
   @Inject({
     attrs: true,
-    parse: false
+    parse: false,
+    sync: ['clear']
   })
   componentDidLoad() {
     this.autocapitalize
     this.native.addEventListener('change', () => {
       this.value = this.native.value;
+    });
+    this.native.addEventListener('keyup', () => {
+      if (this.clearable) this.setClearButtonDisplay();
     });
   }
 
@@ -37,10 +44,31 @@ export class AzInput {
     }
   }
 
+  clear() {
+    this.value = '';
+    this.setClearButtonDisplay();
+  }
+
+  setClearButtonDisplay () {
+    this.clearButton.style.display = this.native.value && this.native.value.length > 0 ? '' : 'none';
+  }
+
   render() {
-    return [
-      (this.caption && <span class="az-input-caption az-caption">{this.caption}</span>),
-      <input ref={el => this.native = el} type={this.type} value={this.value}></input>
-    ];
+    return (<Host>
+      {this.caption && <span class="az-input-caption az-caption">{this.caption}</span>}
+      <input ref={el => this.native = el} type={this.type} value={this.value}
+        autocomplete={this.autocomplete}
+        autocorrect={this.autocorrect}
+        autocapitalize={this.autocapitalize}
+        spellcheck={this.spellcheck}
+      >
+      </input>
+      <az-icon ref={el => this.clearButton = el}
+        style={{display: this.clearable && this.value && this.value.length > 0 ? '' : 'none'}}
+        class="clear"
+        icon="circle-cross"
+        onClick={() => this.clear()}>
+      </az-icon>
+    </Host>);
   }
 }
