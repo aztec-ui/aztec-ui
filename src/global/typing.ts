d16d11885e92b6ba8fd1eeb2c0af956df0bde907
wangweihua <wangweihua@vipkid.com.cn>
2019-11-10 07:40:19 +0800

    :sparkles: (typing.ts) add PositionHorizontal/Vertical

--- src/global/typing.ts
+++ src/global/typing.ts
@@ -1,3 +1,5 @@
 export type ComponentStyle = 'plain' | 'primary' | 'success' | 'info' | 'warning' | 'danger';
 export type ComponentSize = 'extra-small' | 'small' | 'normal' | 'medium' | 'large' | 'extra-large';
 export type CornerPlacement = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+export type PositionHorizontal = 'left' | 'right';
+export type PositionVertical = 'top' | 'bottom';
