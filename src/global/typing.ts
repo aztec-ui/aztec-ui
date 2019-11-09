4524352bc6167eb55be43fabc5f2f33756b83583
aztack <aztack@163.com>
2019-11-09 12:22:30 +0800

    :sparkles: (typing) add CornerPlacement

--- src/global/typing.ts
+++ src/global/typing.ts
@@ -1,2 +1,3 @@
 export type ComponentStyle = 'plain' | 'primary' | 'success' | 'info' | 'warning' | 'danger';
 export type ComponentSize = 'extra-small' | 'small' | 'normal' | 'medium' | 'large' | 'extra-large';
+export type CornerPlacement = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
