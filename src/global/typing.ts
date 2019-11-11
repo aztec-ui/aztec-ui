2422bfc99c121f649abe560856dfddc49a9c1f2f
wangweihua <wangweihua@vipkid.com.cn>
2019-11-11 14:42:20 +0800

    :recycle: (typing.ts) add CenterPlacement,EdgePlacment

--- src/global/typing.ts
+++ src/global/typing.ts
@@ -1,5 +1,10 @@
 export type ComponentStyle = 'plain' | 'primary' | 'success' | 'info' | 'warning' | 'danger';
 export type ComponentSize = 'extra-small' | 'small' | 'normal' | 'medium' | 'large' | 'extra-large';
+
+export type CenterPlacement = 'center';
+export type EdgePlacement = 'top-center' | 'bottom-center' | 'right-center' | 'left-center';
 export type CornerPlacement = 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+export type Placement = EdgePlacement | CornerPlacement | CenterPlacement;
+
 export type PositionHorizontal = 'left' | 'right';
 export type PositionVertical = 'top' | 'bottom';
