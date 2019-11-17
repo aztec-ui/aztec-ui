a9f0ccebf48550385ca7a607d11669d5d38c11b8
aztack <aztack@163.com>
2019-11-17 16:42:02 +0800

    :recycle: (az-radio-group) add items(),clear(); make it more generic

--- src/components/radio-group/az-radio-group.tsx
+++ src/components/radio-group/az-radio-group.tsx
@@ -1,5 +1,6 @@
-import { Component, Prop, Element, Host, h } from '@stencil/core';
+import { Component, Prop, Element, Host, h, Method } from '@stencil/core';
 import { HostElement } from '@stencil/core/dist/declarations';
+import { Inject } from '../../utils/utils';
 
 @Component({
   tag: 'az-radio-group',
@@ -10,17 +11,33 @@ export class AzRadioGroup {
   @Element() el: HostElement;
 
   @Prop() caption: string = '';
+  @Prop() itemSelector: string = 'az-radio';
+  @Prop() itemEvent: string = 'changed';
+  @Prop() itemProp: string = 'checked';
+  @Prop() itemValue: any = false;
 
+  @Inject({
+    sync: ['items']
+  })
   componentDidLoad() {
     this.onItemChanged = this.onItemChanged.bind(this);
-    this.el.addEventListener('changed', this.onItemChanged);
+    this.el.addEventListener(this.itemEvent, this.onItemChanged);
+  }
+
+  items(): HTMLAzRadioElement[] {
+    return Array.from(this.el.querySelectorAll(this.itemSelector)) as HTMLAzRadioElement[] || [];
+  }
+
+  @Method()
+  async clear() {
+    this.items().forEach((item) => {
+      item.setAttribute(this.itemProp, this.itemValue);
+    });
   }
 
   onItemChanged(e: UIEvent) {
-    const items = Array.from(this.el.querySelectorAll('az-radio')) as HTMLAzRadioElement[];
-    if (!items || !items.length) return;
-    items.forEach((radio) => {
-      if (e.target !== radio) radio.checked = false;
+    this.items().forEach((item) => {
+      if (e.target !== item) item.setAttribute(this.itemProp, this.itemValue);
     });
   }
 
