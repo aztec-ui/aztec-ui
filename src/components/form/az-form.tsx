805e8de0a4f235b58923b7c5d9d9d9ee023b0300
wangweihua <wangweihua@vipkid.com.cn>
2019-12-02 18:36:06 +0800

    :sparkles: (az-form) add az-form

---
+++ src/components/form/az-form.tsx
@@ -0,0 +1,76 @@
+import { Component, Prop, Element, h, Method } from '@stencil/core';
+import { HostElement } from '@stencil/core/dist/declarations';
+import { Inject } from '../../utils/utils';
+
+export interface IFormItem {
+  name: string;
+  tag: string;
+  caption?: string;
+  props?: Record<string, any>;
+  children?: IFormItem[]
+}
+
+@Component({
+  tag: 'az-form',
+  styleUrl: 'az-form.styl',
+  shadow: false
+})
+export class AzForm {
+  @Element() el: HostElement;
+
+  @Prop({reflect: true}) caption: string = '';
+  @Prop({reflect: true}) labelPosition: 'left' | 'right' | 'top' = 'left';
+  @Prop() items: IFormItem[] = [];
+
+  @Inject({
+    parse: true,
+    attrs: true,
+    remove: true
+  })
+  componentDidLoad() {}
+
+  @Method()
+  async fromJson(items: IFormItem[]) {
+    this.items = items;
+  }
+
+  @Method()
+  async toJson(detailed: boolean = false) {
+    const form = this.el.children[0];
+    const promises = Array.from(form.children).reduce((all: any[], child: HTMLElement) => {
+      if (child.children.length && typeof child.children[0]['toJson'] === 'function') {
+        const realFormItem = child.children[0];
+        const json = realFormItem['toJson'](detailed);
+        all.push(json);
+      }
+      return all;
+    }, []);
+    return Promise.all(promises);
+  }
+
+  render() {
+    const cap = this.caption ? <span class="az-form__caption az-caption">{this.caption}</span> : null;
+    return [
+      cap,
+      <form class={{
+        'az-form': true,
+        [`label-${this.labelPosition}`]: true
+      }}>
+        <slot name="before"></slot>
+        {this.items.length > 0 ? tagize(this.items) : <slot></slot>}
+        <slot name="after"></slot>
+        <div class="az-form__buttons">
+          <slot name="buttons"></slot>
+        </div>
+      </form>
+    ]
+  }
+}
+
+function tagize(items: IFormItem[]) {
+  return items.map((it: IFormItem) => {
+    return Array.isArray(it) ? tagize(it) : (<az-form-item class="az-form-item">
+      <it.tag name={it.name} {...it.props}>{it.children && it.children.length ? tagize(it.children) : null}</it.tag>
+    </az-form-item>)
+  });
+}
