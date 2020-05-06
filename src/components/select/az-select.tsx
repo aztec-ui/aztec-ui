4bdcd258c340f1de3995eb9fb3a0a3de872fab3e
wangweihua <wangweihua@vipkid.com.cn>
2020-05-06 17:42:20 +0800

    :sparkles: (az-select) add value setter/getter for az-select to get inner select value

--- src/components/select/az-select.tsx
+++ src/components/select/az-select.tsx
@@ -10,7 +10,19 @@ import { HostElement } from "@stencil/core/dist/declarations";
 export class AzSelect {
   @Element() el: HostElement;
   @Inject({children: [HTMLOptionElement, HTMLOptGroupElement]})
+
   componentDidLoad() {
+
+  }
+  constructor() {
+    Object.defineProperty(this.el, 'value', {
+      get() {
+        return this.querySelector('select').value;
+      },
+      set(val) {
+        this.querySelector('select').value = val;
+      }
+    });
   }
   render() {
     return (
